9c75d7231572c1cdb4684c1f98319255ceb7346d
pavelc4 <dimzzy23@gmail.com>
2025-05-03 02:00:36 +0700

    Http Server: my First Http server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 console.log('haloo kita akan belajar membuat server');
+
+const  http = require ('http');
+
+const requestListerner = (requset, response) => {
+    response.setHeader('Content-Type', 'text/html');
+
+
+    response.statusCode = 200;
+    response.end('<H1> haloo HTTP Server</H1>');
+}
+
+const server = http.createServer(requestListerner);
+
+
+const  port = 5000;
+const  host = 'localhost';
+
+
+server.listen(port, host, () => {
+    console.log(`server berjalan pada Https://${host}:${port}`);
+})
